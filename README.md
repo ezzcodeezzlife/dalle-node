e7be12fb5dad13a032a17572289acc74ef8c1c62
ez code ez life <fabian.stehle@informatik.hs-fulda.de>
2022-05-24 14:16:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Get Access
+
+[labs.openai.com/waitlist](https://labs.openai.com/waitlist)
+
+- Go to https://labs.openai.com/
+- Open Network Tab in Developer Tools
+- Type a promt and press "Generate"
+- Look for fetch to https://labs.openai.com/api/labs/tasks
+- In the request header look for authorization then get the Bearer Token
+
+
+# Usage
+```bash
+npm install dalle-node
+```
+```javascript
+import { Dalle } from "dalle-node";
+
+const dalle = new Dalle("sess-xxxxxxxxxxxxxxxxxxxxxxxxx"); // Bearer Token 
+
+const generations = await dalle.generate("a cat driving a car");
+
+console.log(generations)
+```
+
+```
+[
+  {
+    id: 'generation-sCnERSYDPP0Zu14fsdXEcKmL',
+    object: 'generation',
+    created: 1553332711,
+    generation_type: 'ImageGeneration',
+    generation: {
+      image_path: 'https://openailabsprodscus.blob.core.windows.net/private/user-hadpVzldsfs28CwvEZYMUT/generations/generation...'
+    },
+    task_id: 'task-nERkiKsdjVCSZ50yD69qewID',
+    prompt_id: 'prompt-2CtaLQsgUbJHHDoJQy9Lul3T',
+    is_public: false
+  },
+  {
+    id: 'generation-hZWt2Nasrx8R0tJjbaROfKVy',
+    object: 'generation',
+    created: 1553332711,
+    generation_type: 'ImageGeneration',
+    generation: {
+      image_path: 'https://openailabsprodscus.blob.core.windows.net/private/user-hadpVzldsfs28CwvEZYMUT/generations/generation...'
+    },
+    task_id: 'task-nERkiKhjasdSZ50yD69qewID',
+    prompt_id: 'prompt-2CtaLasdUbJHHfoJQy9Lul3T',
+    is_public: false
+  },
+  // 8 more ... 
+]
+```
+
+Blog: [openai.com/blog/dall-e/](https://openai.com/blog/dall-e/)
+
---
+++ dalle-node.js
@@ -0,0 +1,61 @@
+import request from "request";
+
+export class Dalle {
+  constructor(bearerToken) {
+    this.bearerToken = bearerToken;
+    this.url = "https://labs.openai.com/api/labs/tasks";
+  }
+
+  async generate(promt) {
+    return new Promise((resolve, reject) => {
+      request.post(
+        {
+          url: this.url,
+          headers: {
+            Authorization: "Bearer " + this.bearerToken,
+          },
+          json: true,
+          body: {
+            task_type: "text2im",
+            prompt: {
+              caption: promt,
+              batch_size: 10,
+            },
+          },
+        },
+        (error, response, body) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(response.statusCode, body);
+            const taskId = body.id;
+
+            const refreshIntervalId = setInterval(() => {
+                request.get(
+                    {
+                      url: this.url + '/' + taskId,
+                      headers: {
+                        Authorization: "Bearer " + this.bearerToken,
+                      },
+                      json: true,
+                    },
+                    (error, response, body) => {
+                      if (error) {
+                        console.log(error);
+                      } else {
+                        if (body.status === "succeeded") {
+                          const generations = body.generations;
+                          clearInterval(refreshIntervalId);
+                          resolve(generations.data);
+                        }
+                      }
+                    }
+                  );
+
+            }, 3000);
+          }
+        }
+      );
+    });
+  }
+}
