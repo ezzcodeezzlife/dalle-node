ca98ba3a4077cc81b2ef899f6ecf62335ba1efe4
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2022-07-24 21:09:38 +0000

    Convert request library to got

--- dalle-node.js
+++ dalle-node.js
@@ -1,4 +1,4 @@
-import request from "request";
+import got from 'got';
 
 export class Dalle {
   constructor(bearerToken) {
@@ -7,67 +7,42 @@ export class Dalle {
   }
 
   async generate(promt) {
-    return new Promise((resolve, reject) => {
-      request.post(
-        {
-          url: this.url,
-          headers: {
-            Authorization: "Bearer " + this.bearerToken,
-          },
-          json: true,
-          body: {
-            task_type: "text2im",
-            prompt: {
-              caption: promt,
-              batch_size: 4,
-            },
-          },
+    const task = await got.post(this.url, {
+      json: {
+        task_type: "text2im",
+        prompt: {
+          caption: promt,
+          batch_size: 4,
         },
-        (error, response, body) => {
-          if (error) {
-            console.log(error);
-          } else {
-            const taskId = body.id;
+      },
+      headers: {
+        Authorization: `Bearer ${ this.bearerToken }`
+      }
+    }).json();
 
-            const refreshIntervalId = setInterval(() => {
-              this.getTask(taskId).then(task => {
-                switch (task.status) {
-                  case "succeeded":
-                    clearInterval(refreshIntervalId);
-                    resolve(task.generations);
-                  case "rejected":
-                    clearInterval(refreshIntervalId);
-                    resolve(task.status_information);
-                  case "pending":
-                }
-              }).catch(error => {
-                console.log(error);
-              })
-            }, 2000);
-          }
+    const refreshIntervalId = await setInterval(() => {
+      this.getTask(task.id).then(updatedTask => {
+        switch (updatedTask.status) {
+          case "succeeded":
+            clearInterval(refreshIntervalId);
+            return updatedTask.generations
+          case "rejected":
+            clearInterval(refreshIntervalId);
+            return updatedTask.status_information;
+          case "pending":
         }
-      );
-    });
+      }).catch(error => {
+        console.log(error);
+        return error;
+      })
+    }, 2000);
   }
 
   async getTask(taskId) {
-    return new Promise((resolve, reject) => {
-      request.get(
-        {
-          url: `${ this.url }/${ taskId }`,
-          headers: {
-            Authorization: "Bearer " + this.bearerToken,
-          },
-          json: true,
-        },
-        (error, response, body) => {
-          if (error) {
-            return reject(error)
-          } else {
-            return resolve(body)
-          }
-        }
-      );
-    });
+    return await got.get(`${ this.url }/${ taskId }`, {
+      headers: {
+        Authorization: "Bearer " + this.bearerToken,
+      },
+    }).json();
   }
 }
